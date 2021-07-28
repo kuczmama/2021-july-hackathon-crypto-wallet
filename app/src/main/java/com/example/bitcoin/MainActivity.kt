38a0e064c72306b8914f18b0b09d121b59dac68e
markkucz <markkucz@amazon.com>
2021-07-28 11:41:50 -0700

    Initial Commit
    This initial commit sets up an empty project for a base bitcoin wallet.
    It is in Kotlin, and it will load the BitcoinJ library, and it syncs
    the blockchain.  It also requests the desired permissions to get the app
    working.

---
+++ app/src/main/java/com/example/bitcoin/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.example.bitcoin
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import org.bitcoinj.core.*
+import org.bitcoinj.core.listeners.DownloadProgressTracker
+import org.bitcoinj.kits.WalletAppKit
+import org.bitcoinj.params.MainNetParams
+import org.bitcoinj.params.TestNet3Params
+import org.bitcoinj.wallet.Wallet
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    final val TAG = "Bitcoin!!"
+    final val IS_PRODUCTION = false
+    private var parameters: NetworkParameters? = null
+    private var walletAppKit: WalletAppKit? = null
+
+    // Only supports legacy addresses
+    final val bitcoinAddress = "myxWv5jFrezuxM6gPvAM77jPPQfM3nLvDE"
+    var mainText: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mainText = findViewById(R.id.main_text)
+        toast("Loading...")
+
+        init()
+    }
+
+    private fun init() {
+        parameters = if (IS_PRODUCTION) MainNetParams.get() else TestNet3Params.get()
+        val forwardingAddress: Address = LegacyAddress.fromBase58(parameters, bitcoinAddress)
+
+        Log.d(TAG, "Forwarding address: $forwardingAddress");
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1);
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults[0] === PackageManager.PERMISSION_GRANTED
+                ) {
+                    Log.d(TAG, "permission granted!")
+                    createWallet()
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Permission denied to read your External storage",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                return
+            }
+        }
+    }
+
+    private fun toast(str: String) {
+        Toast.makeText(
+            this@MainActivity,
+            str,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun createWallet() {
+        Log.d(TAG, "checking permission")
+        parameters = if (IS_PRODUCTION) MainNetParams.get() else TestNet3Params.get()
+        // Download the block chain and wait until it's done.
+        Log.d(TAG, "syncing blockchain")
+        walletAppKit = WalletAppKit(parameters, cacheDir, "MyWallet")
+        walletAppKit?.setDownloadListener(object : DownloadProgressTracker() {
+            override fun progress(pct: Double, blocksSoFar: Int, date: Date?) {
+                super.progress(pct, blocksSoFar, date)
+                val percentage = pct.toInt()
+                Log.d(TAG, "percentage synced: $percentage")
+            }
+
+            override fun doneDownload() {
+                super.doneDownload()
+                Log.d(TAG, "Download complete!")
+
+            }
+        })
+        walletAppKit?.setBlockingStartup(false)
+        walletAppKit?.startAsync()
+
+
+    }
+}
