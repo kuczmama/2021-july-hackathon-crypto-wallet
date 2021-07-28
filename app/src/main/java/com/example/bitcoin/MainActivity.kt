72d3ac5eead1383f4be338ad3a0202ff3c8278c7
markkucz <markkucz@amazon.com>
2021-07-28 14:15:28 -0700

    Hookup send/receive buttons to open their respective activity
    Create a new activity for both sending and receiving
    
    Also, disable the send/receive buttons until the wallet has completely
    loaded... We may consider adding these activities to Fragments for performance
    but this should be fast enough for now.

--- app/src/main/java/com/example/bitcoin/MainActivity.kt
+++ app/src/main/java/com/example/bitcoin/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.bitcoin
 
 import android.Manifest
-import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var receiveButton: Button
     private lateinit var balanceText: TextView
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -42,12 +43,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init() {
+        sendButton.isEnabled = false
+        receiveButton.isEnabled = false
         sendButton.setOnClickListener {
-            toast("Send button clicked")
+            val intent = Intent(this, SendActivity::class.java)
+            startActivity(intent)
         }
 
         receiveButton.setOnClickListener {
-            toast("Receive button clicked!")
+            val intent = Intent(this, ReceiveActivity::class.java).apply {
+                putExtra(Constants.RECEIVE_ADDRESS_KEY.toString(), walletAddress.toString())
+            }
+
+            startActivity(intent)
         }
 
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1);
@@ -112,7 +120,12 @@ class MainActivity : AppCompatActivity() {
                 walletAddress = wallet.freshReceiveAddress()
                 Log.d(TAG, "Wallet Address: $walletAddress")
                 Log.d(TAG, "Balance: ${wallet.balance}")
-                setText(balanceText, "${wallet.balance} sats")
+
+                runOnUiThread {
+                    balanceText.text = "${wallet.balance} sats"
+                    sendButton.isEnabled = true
+                    receiveButton.isEnabled = true
+                }
 
                 wallet.addCoinsReceivedEventListener { wallet1: Wallet?, tx: Transaction, prevBalance: Coin?, newBalance: Coin ->
                     Log.d(TAG, "Tx received Balance: ${wallet.balance}")
